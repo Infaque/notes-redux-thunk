db52558e8dc548e85f9cb49c1ca07657798bead5
Bilal Shahid <bilalshahid705@gmail.com>
2021-05-05 15:15:12 +0500

    Thunk Done

---
+++ src/components/AddNoteForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addNewNote } from "../store/actions";
+function AddNoteForm(props) {
+  const [form, setForm] = useState({
+    title: "",
+    text: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    let newForm = { ...form };
+    newForm[name] = value;
+
+    setForm(newForm);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addNewNote(form);
+    setForm({ title: "", text: "" });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a Note</h4>
+      <div>
+        <label>Title</label>
+        <input
+          name="title"
+          value={form.title}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Text</label>
+        <textarea
+          name="text"
+          value={form.text}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <button className="success">Add Note + </button>
+      </div>
+    </form>
+  );
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addNewNote: (payload) => {
+      console.log(payload);
+      dispatch(addNewNote(payload));
+    },
+  };
+}
+
+export default connect(null, mapDispatchToProps)(AddNoteForm);
---
+++ src/components/Note.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { deleteNote } from "../store/actions";
+
+function Note(props) {
+  return (
+    <div class="note">
+      <h4>{props.title}</h4>
+      <p>{props.text}</p>
+
+      <button class="delete" onClick={() => props.remove(props.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    remove: (id) => dispatch(deleteNote(id)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Note);
